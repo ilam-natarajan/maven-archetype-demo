83882386daba6a8b09ed7a13b333913f100e5bec
Ilamparithi <ilamparithi.natarajan@wipro.com>
2020-06-26 18:08:09 +0100

    add spring boot test

--- src/test/java/com/archetypetraining/demo/DemoApplicationTests.java
+++ src/test/java/com/archetypetraining/demo/DemoApplicationTests.java
@@ -1,13 +1,40 @@
 package com.archetypetraining.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import java.util.Objects;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class DemoApplicationTests {
+	private static final String HTTP_URL = "http://localhost:%d%s";
+	@LocalServerPort
+	private int port;
+	final private TestRestTemplate restTemplate = new TestRestTemplate();
 
 	@Test
-	void contextLoads() {
+	void testHelloController() {
+		final ResponseEntity<String> exchange =
+				restTemplate.exchange(
+						format(HTTP_URL, port, "/hello"),
+						HttpMethod.GET,
+						new HttpEntity<>(new HttpHeaders()),
+						String.class);
+		assertTrue(Objects.requireNonNull(exchange.getBody()).contains("hello"));
+		System.out.println("test success !");
 	}
 
 }
